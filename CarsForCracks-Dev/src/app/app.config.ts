3de8ee576a84000eb5fb4fd51e33fb42ac31f4e6
AxelDMH <axeldmh.270903@gmail.com>
2024-06-25 16:51:15 -0600

    IDK

--- CarsForCracks-Dev/src/app/app.config.ts
+++ CarsForCracks-Dev/src/app/app.config.ts
@@ -13,5 +13,8 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(), provideFirebaseApp(() => initializeApp({ "projectId": "carsforcracks", "appId": "1:112719744658:web:6c6789ae2803eedc1c604b", "storageBucket": "carsforcracks.appspot.com", "apiKey": "AIzaSyCuL3j1vc5oliijA8tLi1y7o1bOdgyIpAs", "authDomain": "carsforcracks.firebaseapp.com", "messagingSenderId": "112719744658", "measurementId": "G-PDZWY6TBWD" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
     })]
 };
