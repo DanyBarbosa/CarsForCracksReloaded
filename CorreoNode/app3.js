66fb4c8891f3949dfb1a16d62aded79d9d7349c6
Einar115 <einarnaim@hotmail.com>
2024-06-25 21:11:36 -0600

    Qr y nodeJS

---
+++ CorreoNode/app3.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3002;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/random-number', (req, res) => {
+    const randomNumber = Math.floor(Math.random() * 12) + 1;
+    res.json({ number: randomNumber });
+});
+
+app.listen(port, () => {
+    console.log(`API listening at http://localhost:${port}`);
+});
