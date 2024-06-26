8607627d6f82891bf3055d251e84f4faf0f8f2a5
AxelDMH <axeldmh.270903@gmail.com>
2024-06-25 21:50:16 -0600

    Cambios finales

--- CarsForCracks-Dev/src/app/registro/registro.component.ts
+++ CarsForCracks-Dev/src/app/registro/registro.component.ts
@@ -81,7 +81,7 @@ export class RegistroComponent {
           this.datosCliente.fechaInicio = this.startDate;
           this.datosCliente.fechaFin = this.endDate;
 
-          this.http.post('http://localhost:3001/registro', { email: this.usuario.correo, datosCliente: this.datosCliente })
+          this.http.post('http://localhost:3000/registro', { email: this.usuario.correo, datosCliente: this.datosCliente })
           .subscribe(
             response => {
               console.log('Respuesta del servidor:', response);
